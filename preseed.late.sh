ccfa44fc354b17286d0c12e53daabdd1c89ce63b
Nikita B. Zuev <nikitazu@gmail.com>
2012-09-06 23:54:20 +0400

    Fixed copying of ssh public key

--- preseed.late.sh
+++ preseed.late.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-source /qvm.config
+. /qvm.config
 
 ### SETUP SSH
 ### #########
@@ -11,30 +11,31 @@ QVM_SSH_CFG="/target/etc/ssh/sshd_config"
 
 test -f /qvm.config && echo "qvm.config found and that is good">>/target/root/qvm.log
 test -f "/$QVM_SSH_KEY" && echo "$QVM_SSH_KEY found and that is good">>/target/root/qvm.log
+test -f /usr/bin/perl && echo "perl found and that is cool">>/target/root/qvm.log
 
 echo making ssh dir
 mkdir -p /target/root/.ssh
 
 echo copying ssh public key
-cp "/$QVM_SSH_KEY" "$QVM_SSH_HOME/$QVM_SSH_KEY" #fixme
+cp "/$QVM_SSH_KEY" "$QVM_SSH_HOME/$QVM_SSH_KEY"
 
 echo authorizing ssh public key
-cat "/$QVM_SSH_KEY" >>"$QVM_SSH_HOME"/authorized_keys #fixme
+cat "/$QVM_SSH_KEY" >>"$QVM_SSH_HOME"/authorized_keys
 
 echo allowing root only acces to key
 chmod 600 -R "$QVM_SSH_HOME"
 
 echo setting up ssh
-perl -pi -e "s/Port 22/Port $QVM_SSH_PORT/" $QVM_SSH_CFG
-perl -pi -e 's/PermitRootLogin yes/PermitRootLogin yes/' $QVM_SSH_CFG
-perl -pi -e 's/#PasswordAuthentication yes/PasswordAuthentication yes/' $QVM_SSH_CFG
-perl -pi -e 's/#AuthorizedKeysFile     %h/.ssh/authorized_keys/AuthorizedKeysFile     %h/.ssh/authorized_keys/' $QVM_SSH_CFG #wontwork?
+# perl is not available fixme
+/usr/bin/perl -pi -e "s/Port 22/Port $QVM_SSH_PORT/" $QVM_SSH_CFG
+/usr/bin/perl -pi -e 's/PermitRootLogin yes/PermitRootLogin yes/' $QVM_SSH_CFG
+/usr/bin/perl -pi -e 's/#PasswordAuthentication yes/PasswordAuthentication yes/' $QVM_SSH_CFG
+/usr/bin/perl -pi -e 's/#AuthorizedKeysFile     %h/.ssh/authorized_keys/AuthorizedKeysFile     %h/.ssh/authorized_keys/' $QVM_SSH_CFG #wontwork?
 
 
 ### ALL HAIL GEEKS
 ### ##############
 
 echo geekrulez>/target/root/geekrulez
-echo "$QVM_HOST">>/target/root/geekrulez
 echo "all is done at `date`">>/target/root/qvm.log
 
